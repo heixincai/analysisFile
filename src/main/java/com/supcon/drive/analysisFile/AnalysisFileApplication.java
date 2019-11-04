4af8298041358ed944790479d61cdfa6a5c87163
SYD <957548804@qq.com>
2019-11-04 20:14:24 +0800

    新增springboot项目

---
+++ src/main/java/com/supcon/drive/analysisFile/AnalysisFileApplication.java
@@ -0,0 +1,13 @@
+package com.supcon.drive.analysisFile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalysisFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalysisFileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/supcon/drive/analysisFile/AnalysisFileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supcon.drive.analysisFile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalysisFileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
